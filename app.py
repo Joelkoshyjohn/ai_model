103c6970e88d0f98b219e88ce5160db604b7e809
Joelkoshyjohn <120305719+Joelkoshyjohn@users.noreply.github.com>
2026-03-08 16:09:02 +0530

    Added CORS support

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 
 app = FastAPI()
 
-# Load model and vectorizer
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow all origins (good for testing)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load model
 model = joblib.load("model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
-# Request format
 class Review(BaseModel):
     text: str
 
-
 @app.get("/")
 def home():
-    return {"message": "Fake Review Detection API Running"}
-
+    return {"message": "AI Review Detector Running"}
 
 @app.post("/predict")
 def predict(review: Review):
@@ -26,10 +33,8 @@ def predict(review: Review):
     prediction = model.predict(review_vector)[0]
     probability = model.predict_proba(review_vector)[0][1]
 
-    result = "Fake Review" if prediction == 1 else "Genuine Review"
-
     return {
         "review": review.text,
-        "prediction": result,
+        "prediction": int(prediction),
         "ai_probability": float(probability)
     }
